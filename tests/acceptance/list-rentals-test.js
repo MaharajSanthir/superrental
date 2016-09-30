3fcc4bfb04e77da1568502dc234f9228ff376316
Maharaj San <msanthir@gmail.com>
2016-09-29 22:17:29 -0400

    acceptance test

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,10 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | list rentals');
+test('should redirect to rentals route', function (assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/', 'Visit Passed');
+  });
+});
